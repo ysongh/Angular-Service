f62d38e4979e75abe2e2161ce45dcdb091378f5e
Song <yousonghou@yahoo.com>
2018-10-29 02:40:39 +0000

    add counter

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,21 +1,24 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 
-//@Injectable()
+import { CounterService } from './counter.service';
+
+@Injectable()
 export class UsersService{
   activeUsers = ['Song', 'Roy'];
   inactiveUsers = ['Coke', 'Ike'];
   
-  userSetToInactive = new EventEmitter<number>();
-  userSetToActive = new EventEmitter<number>();
+  constructor(private counterService: CounterService){}
   
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counterService.incrementActiveToInactive;
   }
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.counterService.incrementInactiveToActive;
   }
   
 }
